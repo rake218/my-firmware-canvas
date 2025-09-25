7d9142ea07f0b5ca60faea8b3aaea659b1578f79
Rakesh Narayanan <rakeshanmenon@gmail.com>
2025-09-25 06:52:04 +0200

    Delete challenges/001-helloworld directory

--- challenges/001-helloworld/src/helloworld.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-    printf("Hello, Firmware Canvas!\n");
-    return 0;
-}
--- challenges/001-helloworld/tests/test_helloworld.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-gcc ../src/helloworld.c -o helloworld
-./helloworld | grep "Hello, Firmware Canvas!"
