0786f2ae34925e2104d4cd2f1340cca4c190b661
Rakesh Narayanan <rakeshanmenon@gmail.com>
2025-12-28 07:21:44 +0000

    fix(challenge-005): configure freestanding bare-metal build for STM32F7
    Update toolchain and CMake configuration to use -ffreestanding and -nostdlib,
    so the ARM cross-compiler can build without linking host libc symbols.
    Fixes CMake compiler check failures for bare-metal STM32F769 firmware.

--- tools/arm-none-eabi.cmake
+++ tools/arm-none-eabi.cmake
@@ -4,3 +4,8 @@ set(CMAKE_SYSTEM_PROCESSOR arm)
 set(CMAKE_C_COMPILER arm-none-eabi-gcc)
 set(CMAKE_ASM_COMPILER arm-none-eabi-gcc)
 set(CMAKE_OBJCOPY arm-none-eabi-objcopy)
+
+# Tell CMake this is freestanding
+set(CMAKE_C_FLAGS "-mcpu=cortex-m7 -mthumb -O0 -g -ffreestanding -nostdlib")
+set(CMAKE_ASM_FLAGS "-mcpu=cortex-m7 -mthumb")
+set(CMAKE_EXE_LINKER_FLAGS "-T${CMAKE_SOURCE_DIR}/linker/stm32f769.ld -nostdlib")
