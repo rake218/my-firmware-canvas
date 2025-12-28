eb19ad94bef345d9a8b586672789dd4d365fcb5f
Rakesh Narayanan <rakeshanmenon@gmail.com>
2025-12-28 07:30:23 +0000

    fix duplicate specificaiton of ld file

--- tools/arm-none-eabi.cmake
+++ tools/arm-none-eabi.cmake
@@ -8,4 +8,3 @@ set(CMAKE_OBJCOPY arm-none-eabi-objcopy)
 # Tell CMake this is freestanding
 set(CMAKE_C_FLAGS "-mcpu=cortex-m7 -mthumb -O0 -g -ffreestanding -nostdlib")
 set(CMAKE_ASM_FLAGS "-mcpu=cortex-m7 -mthumb")
-set(CMAKE_EXE_LINKER_FLAGS "-T${CMAKE_SOURCE_DIR}/linker/stm32f769.ld -nostdlib")
