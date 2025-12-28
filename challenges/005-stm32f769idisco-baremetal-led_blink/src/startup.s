8d1575c9d78d0a0d717cb0aa34e861c94a3c057d
Rakesh Narayanan <rakeshanmenon@gmail.com>
2025-12-28 06:52:24 +0000

    updated startup file to minimal cortex m7 version to support thumb 2

--- challenges/005-stm32f769idisco-baremetal-led_blink/src/startup.s
+++ challenges/005-stm32f769idisco-baremetal-led_blink/src/startup.s
@@ -2,31 +2,19 @@
 .cpu cortex-m7
 .thumb
 
-/* Symbols for linker */
+.global _start
 .global Reset_Handler
-.global _estack
 
-/* Vector table: first entry = stack, second = reset handler */
-.section .isr_vector,"a",%progbits
-.word _estack          /* Initial stack pointer */
-.word Reset_Handler    /* Reset handler entry point */
+.section .isr_vector, "a", %progbits
+.word  _estack
+.word  Reset_Handler
 
-.section .text
-
-/* Reset Handler */
+.section .text.Reset_Handler
 Reset_Handler:
-    /* Initialize BSS (zero uninitialized data) */
-    ldr r0, =_sbss       /* Start of BSS */
-    ldr r1, =_ebss       /* End of BSS */
-zero_bss:
-    cmp r0, r1
-    it lt
-    movlt r2, #0
-    strlt r2, [r0], #4   /* Zero each word */
-    blt zero_bss
+    ldr r0, =_estack
+    mov sp, r0
 
-    /* Call main() function in C */
     bl main
 
-hang:
-    b hang               /* Infinite loop if main returns */
+Hang:
+    b Hang
